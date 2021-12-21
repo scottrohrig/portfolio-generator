977157724c10b375f1ad61b65dd1177b80e10eb4
Scott Rohrig <scott.rohrig@gmail.com>
2021-12-20 18:23:01 -0800

    generate html page

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // PORTFOLIO GENERATOR  
 
+// import { writeFile } from 'fs';
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [_name, github] = profileDataArgs;
 
+const pageHTML = generatePage(_name, github);
 
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see changes.');
+});
